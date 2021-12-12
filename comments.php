bf1ccee5df118e162e3c254c5311c0c0b5fb0ef5
dyCodes <yusufdaudu51@gmail.com>
2021-12-12 03:56:28 +0100

    Update comment template

--- comments.php
+++ comments.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying comments
  *
@@ -15,35 +16,27 @@
  * the visitor has not yet entered the password we will
  * return early without loading the comments.
  */
-if ( post_password_required() ) {
+if (post_password_required()) {
 	return;
 }
 ?>
 
-<div id="comments" class="comments-area">
+<div id="comments" class="blog-comments mt-4">
 
 	<?php
 	// You can start editing here -- including this comment!
-	if ( have_comments() ) :
-		?>
-		<h2 class="comments-title">
-			<?php
-			$media_blog_comment_count = get_comments_number();
-			if ( '1' === $media_blog_comment_count ) {
-				printf(
-					/* translators: 1: title. */
-					esc_html__( 'One thought on &ldquo;%1$s&rdquo;', 'media-blog' ),
-					'<span>' . wp_kses_post( get_the_title() ) . '</span>'
-				);
+	if (have_comments()) :
+	?>
+		<h2 class="comments-count mb-3">
+			<?php $media_blog_comment_count = get_comments_number();
+			if ('1' === $media_blog_comment_count) {
+				echo '<span>' . esc_html__('1 Comment', 'media-blog') . '</span>';
 			} else {
-				printf( 
-					/* translators: 1: comment count number, 2: title. */
-					esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $media_blog_comment_count, 'comments title', 'media-blog' ) ),
-					number_format_i18n( $media_blog_comment_count ), // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-					'<span>' . wp_kses_post( get_the_title() ) . '</span>'
+				printf(
+					'<span>' . esc_html__('%1$s Comments', 'media-blog') . '</span>',
+					$media_blog_comment_count
 				);
-			}
-			?>
+			} ?>
 		</h2><!-- .comments-title -->
 
 		<?php the_comments_navigation(); ?>
@@ -52,7 +45,7 @@ if ( post_password_required() ) {
 			<?php
 			wp_list_comments(
 				array(
-					'style'      => 'ol',
+					'style' => 'ol',
 					'short_ping' => true,
 				)
 			);
@@ -63,15 +56,30 @@ if ( post_password_required() ) {
 		the_comments_navigation();
 
 		// If comments are closed and there are comments, let's leave a little note, shall we?
-		if ( ! comments_open() ) :
-			?>
-			<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'media-blog' ); ?></p>
-			<?php
+		if (!comments_open()) :
+		?>
+			<p class="no-comments"><?php esc_html_e('Comments are closed.', 'media-blog'); ?></p>
+	<?php
 		endif;
 
 	endif; // Check for have_comments().
 
-	comment_form();
+	$comments_args = array(
+		//Define Fields
+		'fields' => array(
+			//Author field
+			'author' => '<div class="form-group mb-3"><input name="author" type="text" class="form-control" aria-required="true" placeholder="Your Name*"></div>',
+			//Email Field
+			'email' => '<div class="form-group mb-3"><input name="email" type="text" class="form-control" placeholder="Your Email*"></div>',
+			//URL Field
+			'url' => '<div class="form-group mb-3"><input id="url" name="url" type="text" class="form-control" placeholder="Your Website"></div>',
+			//Cookies
+			'cookies' => '<div class="form-group mb-3"><input id="cookies" type="checkbox" required><label for="cookies"> By commenting you accept the<a href="' . get_privacy_policy_url() . '"> Privacy Policy </a></label></div>',
+		),
+		'comment_field' => '<div class="form-group mb-3"><textarea name="comment" rows="8" class="form-control" placeholder="Your Comment*"></textarea></div>',
+	);
+
+	comment_form($comments_args);
 	?>
 
 </div><!-- #comments -->
