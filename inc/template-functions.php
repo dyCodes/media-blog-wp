761856598e5c52f55022cd6721e3c642be03663a
dyCodes <yusufdaudu51@gmail.com>
2021-12-12 03:58:36 +0100

    Add excerpt_more filter

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Functions which enhance the theme by hooking into WordPress
  *
@@ -11,27 +12,34 @@
  * @param array $classes Classes for the body element.
  * @return array
  */
-function media_blog_body_classes( $classes ) {
-	// Adds a class of hfeed to non-singular pages.
-	if ( ! is_singular() ) {
-		$classes[] = 'hfeed';
-	}
+function media_blog_body_classes($classes)
+{
 
 	// Adds a class of no-sidebar when there is no sidebar present.
-	if ( ! is_active_sidebar( 'sidebar-1' ) ) {
+	if (!is_active_sidebar('sidebar-1')) {
 		$classes[] = 'no-sidebar';
 	}
 
 	return $classes;
 }
-add_filter( 'body_class', 'media_blog_body_classes' );
+add_filter('body_class', 'media_blog_body_classes');
 
 /**
  * Add a pingback url auto-discovery header for single posts, pages, or attachments.
  */
-function media_blog_pingback_header() {
-	if ( is_singular() && pings_open() ) {
-		printf( '<link rel="pingback" href="%s">', esc_url( get_bloginfo( 'pingback_url' ) ) );
+function media_blog_pingback_header()
+{
+	if (is_singular() && pings_open()) {
+		printf('<link rel="pingback" href="%s">', esc_url(get_bloginfo('pingback_url')));
 	}
 }
-add_action( 'wp_head', 'media_blog_pingback_header' );
+add_action('wp_head', 'media_blog_pingback_header');
+
+/**
+ * Change the excerpt more string
+ */
+function media_blog_excerpt_more($more)
+{
+	return '...';
+}
+add_filter('excerpt_more', 'media_blog_excerpt_more');
